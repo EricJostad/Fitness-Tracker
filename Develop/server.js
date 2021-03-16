33d7544a520db9338f4338586ccadf8928c6bbfd
EJostad <eric.jostad@outlook.com>
2021-03-15 22:03:59 -0700

    Set up boilerplate code for server.js file.

---
+++ Develop/server.js
@@ -0,0 +1,15 @@
+// This creates the express server
+const express = require('express');
+const mongoose = require('mongoose');
+
+const PORT = process.env.PORT || 1170;
+const app = express();
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/Choresdb', { useNewUrlParser: true });
+
+mongoose.connection.on('error', (err) => console.log(`error in mongoose connection: ${err.message}`));
+
+mongoose.connection.once('open', () => {
+    console.log('mongoose connected!');
+    app.listen(PORT, (err) => console.log(`http://localhost/${PORT}`));
+});
