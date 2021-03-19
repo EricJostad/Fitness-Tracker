4100720175fb299d02a4a1064febc202c25d2ccf
EJostad <eric.jostad@outlook.com>
2021-03-18 22:23:13 -0700

    Updated structure of overall server file.

--- Develop/server.js
+++ Develop/server.js
@@ -1,14 +1,24 @@
 // Setting up required dependencies for server creation
 const express = require('express');
+const morgan = require('morgan');
 const mongoose = require('mongoose');
-const Workout = require('../Develop/models/fitness');
 const apiRoutes = require('../Develop/routes/apiRoutes');
 const htmlRoutes = require('../Develop/routes/htmlRoutes');
 
 const PORT = process.env.PORT || 1170;
 const app = express();
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+});
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+app.use(morgan('dev'));
 
 app.use("/", htmlRoutes);
 app.use("/api", apiRoutes);
@@ -17,5 +27,5 @@ mongoose.connection.on('error', (err) => console.log(`error in mongoose connecti
 
 mongoose.connection.once('open', () => {
     console.log('mongoose connected!');
-    app.listen(PORT, (err) => console.log(`http://localhost/${PORT}`));
+    app.listen(PORT, (err) => console.log(`App is running at: http://localhost/${PORT}`));
 });
